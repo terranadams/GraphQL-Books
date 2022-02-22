62775a67f1c83b6662db7d6ef4b17738893c5f22
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-02-22 13:50:49 -0700

    displaying query data on page

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,13 +12,23 @@ const GET_BOOKS_QUERY = gql`
 `;
 
 const BookList = (props) => {
-  const { loading, error, data } = useQuery(GET_BOOKS_QUERY);
+  const { loading, error, data } = useQuery(GET_BOOKS_QUERY); // this hook is EVERYTHING
+
+  const displayBooks = () => {
+    if (loading) return "Loading...";
+    if (error) return `Error! ${error.message}`;
+    else {
+      return data.books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  };
 
   return (
     <div>
       {console.log(data)}
       <ul id="book-list">
-        <li>Book Name</li>
+        {displayBooks()} {/* this function must be called in forder for it to work */}
       </ul>
     </div>
   );
