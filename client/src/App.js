d317f1fd9a72e472cf9b56c6d0ff9dcaf0bcb668
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-02-22 12:34:51 -0700

    setup apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
+import BookList from "./components/BookList";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  // cache: new InMemoryCache() //  Apollo Client uses this to cache query results after fetching them.
+});
 
 function App() {
   return (
-    <div id="main">
-      <h1>Ninja's Reading List</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
